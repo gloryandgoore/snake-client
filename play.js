d300e7b857add256ee672851c189ea28653997b6
Caseyann <goore_44@hotmail.ca>
2021-12-01 20:48:28 -0500

    import connect from client to play

--- play.js
+++ play.js
@@ -1,28 +1,7 @@
-const net = require("net");
-
-// establishes a connection with the game server
-const connect = function () {
-  const conn = net.createConnection({
-    host: '165.227.47.243',
-    port: 50541
-  });
-conn.on("connect", () => {
-  conn.write("Caseyann")
-});
-  // interpret incoming data as text
-  conn.setEncoding("utf8");
-
-  conn.on("data", (data) => {
-    // code that does something when the connection is first established
-    console.log('Server says: ', data);
-  });
-  return conn;
-};
+const { connect } = require("./client");
 
 console.log("Connecting ...");
 connect();
-
-
 
 
 
