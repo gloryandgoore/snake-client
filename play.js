43aa372f4039066cb09932d090851e14e56c9d7d
Caseyann <goore_44@hotmail.ca>
2021-12-02 00:30:01 -0500

    setupInput added

--- play.js
+++ play.js
@@ -3,5 +3,24 @@ const { connect } = require("./client");
 console.log("Connecting ...");
 connect();
 
+const setupInput = function () {
+  const stdin = process.stdin;
+  stdin.setRawMode(true);
+  stdin.setEncoding("utf8");
+  stdin.resume();
+  stdin.on("data", handleUserInput);
+  return stdin;
+};
 
+const handleUserInput = (key) => {
+  process.stdin.on('data', (key) => {
+  if (key === '\u0003') 
+    console.log('Game Terminated');
+    process.exit();
 
+  }); 
+};
+
+setupInput();
+
+module.exports = {setupInput}
